a1f68ffd644f7c04bc8c5fad720c10d795878a57
Pramod Swami <31487543+rajswami29@users.noreply.github.com>
2024-06-23 20:34:18 +0530

    Update PokemonList.js

--- app/Components/PokemonList.js
+++ app/Components/PokemonList.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const PokemonList = ({ pokemon }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       {pokemon.map((p) => (
         <div
           key={p.name}
